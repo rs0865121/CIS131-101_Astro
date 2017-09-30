25cfd74596e2523910f697eb8541b0d28eef485a
AustinFarrand <af0054085@otc.edu>
2017-09-30 13:32:38 -0500

    Moving Image w/ JavaScript

--- scripts.js
+++ scripts.js
@@ -14,4 +14,22 @@ function submitInfo() {
   document.getElementById('outputObject').innerHTML = ("Your Objects Name: " + object);
   /*(name.value && email.value && phone.value) ? alert("Thank You") : alert("Please fill in all fields");*/
 }
+
+function move(){
+  var elem = document.getElementById('animation');
+  var pos = 0;
+  var id = setInterval(frame, 5);
+  function frame(){
+    if(pos==1900){
+      /*clearInterval();*/
+      pos = 0;
+    }
+    else{
+      pos++;
+      /*elem.style.top = pos +'px';*/
+      elem.style.left = pos + 'px';
+    }
+  }
+}
+
 document.getElementById("submit").addEventListener("click", submitInfo, false);
